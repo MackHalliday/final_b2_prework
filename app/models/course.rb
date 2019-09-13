9710b76fa6905794082ee258fd1e319dca1a5673
MackHalliday <mackenzie.halliday@gmail.com>
2019-09-13 09:19:46 -0600

    setup has_many relationships thru

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,13 @@
 class Course < ApplicationRecord
   validates_presence_of :name
   has_many :course_students
+  has_many :students, through: :course_students
+
+  def students_by_grade
+    self.course_students
+    .joins(:student)
+    .order(grade: :desc)
+    .pluck(:name)
+    .join(", ")
+  end
 end
